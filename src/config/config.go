8dd821618c2740874e63bab37d3201e6dc26445d
Cherryrubim <FredyFeredo@gmail.com>
2022-12-09 20:24:17 -0400

    Var Enviroment Token

--- src/config/config.go
+++ src/config/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"encoding/json"
 
 	"github.com/spf13/viper"
 )
@@ -15,29 +16,41 @@ type Config struct {
 // init tries to read the config from the file, and creates it if it doesn't exist.
 func Init() (Config, error) {
 	//configPath, err := os.UserConfigDir()
-	configPath, err := os.Open("/etc/secrets/")
-	if err != nil {
-		return Config{}, errors.New(fmt.Sprintf("Couldn't get user config dir: %v", err))
-	}
-	viper.SetConfigType("json")
-	viper.SetConfigName("chatgpt")
-	//viper.AddConfigPath(configPath)
-	viper.AddConfigPath(configPath.Name())
+	// configPath, err := os.Open("/etc/secrets/")
+	// if err != nil {
+	// 	return Config{}, errors.New(fmt.Sprintf("Couldn't get user config dir: %v", err))
+	// }
+	// viper.SetConfigType("json")
+	// viper.SetConfigName("chatgpt")
+	// //viper.AddConfigPath(configPath)
+	// viper.AddConfigPath(configPath.Name())
 
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			if err := viper.SafeWriteConfig(); err != nil {
-				return Config{}, errors.New(fmt.Sprintf("Couldn't create config file: %v", err))
-			}
-		} else {
-			return Config{}, errors.New(fmt.Sprintf("Couldn't read config file: %v", err))
-		}
+	// if err := viper.ReadInConfig(); err != nil {
+	// 	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+	// 		if err := viper.SafeWriteConfig(); err != nil {
+	// 			return Config{}, errors.New(fmt.Sprintf("Couldn't create config file: %v", err))
+	// 		}
+	// 	} else {
+	// 		return Config{}, errors.New(fmt.Sprintf("Couldn't read config file: %v", err))
+	// 	}
+	// }
+
+	// var cfg Config
+	// err = viper.Unmarshal(&cfg)
+	// if err != nil {
+	// 	return Config{}, errors.New(fmt.Sprintf("Error parsing config: %v", err))
+	// }
+
+	jsonString := os.Getenv("OPENIA_TOKEN")
+	if jsonString == "" {
+		return Config{}, fmt.Errorf("La variable de entorno CONFIG no está definida")
 	}
 
+	// Convertir la cadena JSON en una estructura de tipo Config
 	var cfg Config
-	err = viper.Unmarshal(&cfg)
+	err := json.Unmarshal([]byte(jsonString), &cfg)
 	if err != nil {
-		return Config{}, errors.New(fmt.Sprintf("Error parsing config: %v", err))
+		return Config{}, fmt.Errorf("Error al convertir la cadena JSON en una estructura de tipo Config: %v", err)
 	}
 
 	return cfg, nil
